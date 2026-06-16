a9dc691ad3fa5aaeee357775a8b4ace162b78322
cvn3443-ctrl <cvn3443@gmail.com>
2026-06-16 19:00:12 +0300

    Update main.py

--- server/main.py
+++ server/main.py
@@ -7,13 +7,13 @@ import logging
 import time
 from contextlib import asynccontextmanager
 
-from quotex_bot import QuotexBot
+from quotex_api import QuotexAPI
 from config import config
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-bot = QuotexBot()
+bot = QuotexAPI()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
